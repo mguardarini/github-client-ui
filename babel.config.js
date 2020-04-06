443ec05d21e785986202e5d30ae7e6bfaed4c1e7
Mauro Guardarini <mauro.guardarini@gmail.com>
2020-04-06 20:11:13 -0300

    added babel module resolver (#1)
    Co-authored-by: Mauro Guardarini <mauro.guardarini@lambda3.com.br>

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@images': './src/images',
+        },
+      },
+    ],
+  ],
+}
---
+++ src/screens/Login/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text
+} from 'react-native';
+
+const Login = () => {
+    return (
+
+       <SafeAreaView>
+           <View>
+               <Text>
+                   Tela de Login
+               </Text>
+           </View>
+       </SafeAreaView> 
+
+    )
+}
+
+export default Login
